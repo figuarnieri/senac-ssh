968338c65e13812c0e46852c872faa39a385b2e0
Filipe Guarnieri <filipe@ibaro.com.br>
2017-10-05 15:32:45 -0300

    Banco de Dados Senac

--- db/bancodedados.php
+++ db/bancodedados.php
@@ -1,8 +1,22 @@
 <?php
-	$db_host = "kanino.database.windows.net";
+	// Banco Interno
+	/*$db_host = "kanino.database.windows.net";
 	$db_name = "kaninos";
 	$db_user = "db_danlucindo";
 	$db_pass = "#Db_1533";
+	$dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
+
+	if(!$db = odbc_connect($dsn, $db_user, $db_pass)){
+
+		echo "ERRO AO CONECTAR AO BANCO DE DADOS";
+		exit();
+	}*/
+
+	// Banco Senac
+	$db_host = "kanino-pi.database.windows.net";
+	$db_name = "Kanino";
+	$db_user = "TSI";
+	$db_pass = "SistemasInternet123";
 	$dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
 
 	if(!$db = odbc_connect($dsn, $db_user, $db_pass)){
