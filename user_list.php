9f295aa0cb8247775a22f8a5d57d64c7bb12f735
Filipe Guarnieri <fyguarnieri@gmail.com>
2017-11-22 21:11:26 -0200

    Add files via upload

--- user_list.php
+++ user_list.php
@@ -2,10 +2,10 @@
 <link rel="stylesheet" href="dist/css/theme/pages/list.min.css">
 
 <main class="main list wrap cf">
-	<div class="pc-col-20">
-		<span class="breadcrumb fl-l fa fa-user-circle">Usuários / Listagem</span><span class="fl-l"><a href="user.php" class="button button-breadcrumb">Cadastrar</a></span>
+	<div class="pc-col-20 t-col-20">
+		<span class="breadcrumb fl-l fa fa-user-circle">Usuários / Listagem</span><span class="fl-l button-breadcrumb-wrap"><a href="user.php" class="button button-breadcrumb">Cadastrar</a></span>
 	</div>
-	<div class="pc-col-20">
+	<div class="pc-col-20 t-col-20">
 		<div class="list--content">
 			<div class="list--pages ta-r">
 				<div class="fl-l list--tools d-n">
@@ -18,18 +18,12 @@
 						</select>
 					</form>
 				</div>
-				<ul class="list--pagelist">
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b"><i class="fa fa-chevron-left"></i></a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b">1</a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b">2</a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b">3</a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b">4</a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b">5</a></li>
-					<li class="list--pagebox"><a href="#" class="list--pagelink d-b"><i class="fa fa-chevron-right"></i></a></li>
-				</ul>
+				<div class="list--pagelist">
+					<div class="list--pagebox"><input type="search" class="list--input d-b" placeholder="Filtrar Listagem"></div>
+				</div>
 			</div>
 			<table class="list--table" cellpadding="0" cellspacing="0">
-				<tr>
+				<tr class="t-d-n">
 					<th class="ta-c" width="50">
 						<input type="checkbox" name="SelecioneTodos" id="SelecioneTodos" data-multichange="all">
 						<label for="SelecioneTodos" class="fa fa-square-o"></label>
@@ -49,16 +43,16 @@
 				");
 				while($user = odbc_fetch_array($user_sql)){ ?>
 					<tr>
-						<td class="ta-c">
+						<td class="ta-c t-d-n">
 							<input type="checkbox" name="multicheck[]" id="id<?php echo $user['idUsuario']?>" value="<?php echo $user['idUsuario']?>">
 							<label for="id<?php echo $user['idUsuario']?>" class="fa fa-square-o"></label>
 						</td>
-						<td><?php echo $user['nomeUsuario']?></td>
-						<td><?php echo $user['loginUsuario']?></td>
-						<td class="ta-c">
+						<td t-pseudo-before="Nome: " class="t-d-b"><?php echo $user['nomeUsuario']?></td>
+						<td t-pseudo-before="Login: " class="t-d-b"><?php echo $user['loginUsuario']?></td>
+						<td t-pseudo-before="Status: <?php echo $user['usuarioAtivo'] ? 'Ativo' : 'Inativo'?> " class="ta-c t-d-b t-ta-l">
 							<i class="fa <?php echo $user['usuarioAtivo'] ? 'fa-check-circle' : 'fa-times-circle'?>" data-user-status="<?php echo $user['idUsuario']?>" data-tipfy="<?php echo $user['usuarioAtivo'] ? 'Ativo' : 'Inativo'?>" data-tipfy-side="left"></i>
 						</td>
-						<td class="ta-c">
+						<td class="ta-c t-d-b list--icons">
 							<a class="fa fa-pencil d-ib" href="user.php?edit=<?php echo $user['idUsuario']?>"></a>
 							<a class="fa fa-trash d-ib" href="includes/user_delete.php?id=<?php echo $user['idUsuario']?>"></a>
 						</td>
